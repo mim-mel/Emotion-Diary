99e13ffb624ec8cc668b155b2cf7b878f090beb1
mim-mel <mfblade2014@gmail.com>
2022-08-26 16:56:41 +0900

    feat : build 파일 생성

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react"; 
 import DiaryEditior from "../components/DiaryEditior";
 
 const New = ()=>{
+
+    useEffect(()=>{
+        const titleElement = document.getElementsByTagName('title')[0];
+        titleElement.innerHTML = `감정 일기장 - 새 일기`
+    },[]);
+
     return(
         <div>
             <DiaryEditior />
